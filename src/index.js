9e5fb6d976afe78125aea072415bc6941376c643
TPLeticia <leticiatp@live.com.mx>
2025-05-03 12:31:32 -0600

    server working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('clase 13');
+import express from 'express';
+
+import dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor a la escucha en http://localhost:${PORT}`);
+} );
